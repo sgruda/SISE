94c7b24505628920b24278f7d064748fd3c24883
sgruda <szymon_gruda@o2.pl>
2020-03-13 13:32:52 +0100

    Add FileData() method.

--- Zad1/src/main/java/pl/lodz/p/it/io/FileService.java
+++ Zad1/src/main/java/pl/lodz/p/it/io/FileService.java
@@ -1,9 +1,12 @@
 package pl.lodz.p.it.io;
 
+import com.sun.tools.javac.util.Pair;
 import lombok.Getter;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Scanner;
 
 
@@ -16,7 +19,7 @@ public class FileService {
     private final String ALGORITHM_FILE;
     private int ROW_NUMBER;
     private int COLUMN_NUMBER;
-
+    private byte[][] puzzle;
 
 
     public FileService(String PUZZLE_FILE, String SOLVED_FILE, String ADDITIONAL_DATA_FILE, String ALGORITHM_FILE) {
@@ -24,9 +27,10 @@ public class FileService {
         this.SOLVED_FILE = SOLVED_FILE;
         this.ADDITIONAL_DATA_FILE = ADDITIONAL_DATA_FILE;
         this.ALGORITHM_FILE = ALGORITHM_FILE;
+        this.loadData();
     }
 
-    public byte[][] getPuzzle() {
+    private void loadData() {
         File file = new File(this.PUZZLE_FILE);
         try (Scanner sc = new Scanner(file)) {
             int line = 0;
@@ -38,17 +42,33 @@ public class FileService {
                     puzzle = new byte[this.ROW_NUMBER][this.COLUMN_NUMBER];
                 } else {
                     int i = 0;
-                    while(sc.hasNextByte()) {
-                        puzzle[line][i] = sc.nextByte();
+                    while(i < this.COLUMN_NUMBER) {
+                        puzzle[line - 1][i] = sc.nextByte();
                         i++;
                     }
                 }
                 line++;
             }
-            return puzzle;
+            this.puzzle = puzzle;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-    return null;
+    }
+    public void saveData(final byte[][] solvedPuzzle)  {
+        File file = new File(this.SOLVED_FILE);
+        try (FileOutputStream fout = new FileOutputStream(file)) {
+            byte i = 0;
+            for(byte [] b : solvedPuzzle) {
+                for(byte b2 : b) {
+                    fout.write(String.valueOf(b2).getBytes());
+                    i++;
+                }
+                fout.write(System.getProperty("line.separator").getBytes());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
