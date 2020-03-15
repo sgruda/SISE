9a0e5b376b4f986490f81d01c8ab9e6b1161413a
sgruda <szymon_gruda@o2.pl>
2020-03-15 14:35:14 +0100

    Add AlgorithmFactory and implementation its in App

--- Zad1/src/main/java/pl/lodz/p/it/App.java
+++ Zad1/src/main/java/pl/lodz/p/it/App.java
@@ -1,15 +1,23 @@
 package pl.lodz.p.it;
 
 
-import pl.lodz.p.it.algorithm.AlgorithmBFS;
+import pl.lodz.p.it.algorithm.Algorithm;
 import pl.lodz.p.it.io.FileService;
 
 import java.io.IOException;
 
 public class App {
     public static void main( String[] args ) throws IOException {
-        FileService fileService = new FileService(args[2], args[3], "","");
-        AlgorithmBFS algorithmBfs = new AlgorithmBFS(fileService.getCOLUMN_NUMBER(), fileService.getROW_NUMBER(), fileService.getPuzzle(), args[1]);
-        fileService.saveData(algorithmBfs.solve());
+        /*  ARGS
+            0 - Acronym of algorithm
+            1 - Order of searching in algorithms: BFS, DFS
+                Acronym of heuristics
+            2 - Path to file with puzzle to solve
+            3 - Path to file for solved puzzle
+            4 - Path to file for additional data
+         */
+        FileService fileService = new FileService(args[2], args[3], args[4]);
+        Algorithm algorithm = AlgorithmFactory.getAlgorithm(args, fileService);
+        fileService.saveData(algorithm.solve());
     }
 }
