5a025af944189d3ac6e093681c7dfb5610944fa3
sgruda <szymon_gruda@o2.pl>
2020-03-18 13:06:48 +0100

    Small Refactoring

--- Zad1/src/main/java/pl/lodz/p/it/algorithm/AlgorithmBFS.java
+++ Zad1/src/main/java/pl/lodz/p/it/algorithm/AlgorithmBFS.java
@@ -1,17 +1,14 @@
 package pl.lodz.p.it.algorithm;
 
-import com.sun.tools.javac.util.Pair;
 import pl.lodz.p.it.enums.Direction;
 
 public class AlgorithmBFS extends Algorithm {
 
     private final String searchOrder;
-//    private int directionCharIndex;
 
     public AlgorithmBFS(int ROW_NUMBER, int COLUMN_NUMBER, int[][] puzzles, String searchOrder){
         super(ROW_NUMBER, COLUMN_NUMBER, puzzles);
         this.searchOrder = searchOrder.toUpperCase();
-//        directionCharIndex = -1;
     }
 
     @Override
