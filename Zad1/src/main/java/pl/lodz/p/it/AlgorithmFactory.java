24bf6ea21157c1c204ddfbe6481c8cbc79ebccb7
Szymon Gruda <216773@edu.p.lodz.pl>
2020-03-21 16:11:57 +0100

    Upgrade AlgorithmFactory

--- Zad1/src/main/java/pl/lodz/p/it/AlgorithmFactory.java
+++ Zad1/src/main/java/pl/lodz/p/it/AlgorithmFactory.java
@@ -1,8 +1,11 @@
 package pl.lodz.p.it;
 
 import pl.lodz.p.it.algorithm.Algorithm;
+import pl.lodz.p.it.algorithm.AlgorithmAstar;
 import pl.lodz.p.it.algorithm.AlgorithmBFS;
 import pl.lodz.p.it.algorithm.AlgorithmDFS;
+import pl.lodz.p.it.algorithm.heuristic.HammingHeuristic;
+import pl.lodz.p.it.algorithm.heuristic.ManhattanHeuristic;
 import pl.lodz.p.it.io.FileService;
 
 public class AlgorithmFactory {
@@ -16,7 +19,14 @@ public class AlgorithmFactory {
                 return new AlgorithmDFS(fileService.getCOLUMN_NUMBER(), fileService.getROW_NUMBER(), fileService.getPuzzle(), args[1]);
             case "astr":
             case "ASTR":
-                return null; //TODO
+                switch (args[1]) {
+                    case "manh":
+                    case "MANH":
+                        return new AlgorithmAstar(fileService.getCOLUMN_NUMBER(), fileService.getROW_NUMBER(),fileService.getPuzzle(), new ManhattanHeuristic());
+                    case "hamm":
+                    case "HAMM":
+                        return new AlgorithmAstar(fileService.getCOLUMN_NUMBER(), fileService.getROW_NUMBER(),fileService.getPuzzle(), new HammingHeuristic());
+                }
         }
         return null;
     }
