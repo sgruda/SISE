29768645438498d05abd4d20be88be71925a10b7
Szymon Gruda <216773@edu.p.lodz.pl>
2020-03-21 15:19:08 +0100

    Implemented method solve() in AlgorithmAstar

--- Zad1/src/main/java/pl/lodz/p/it/algorithm/AlgorithmAstar.java
+++ Zad1/src/main/java/pl/lodz/p/it/algorithm/AlgorithmAstar.java
@@ -1,10 +1,12 @@
 package pl.lodz.p.it.algorithm;
 
+import lombok.Getter;
 import pl.lodz.p.it.algorithm.heuristic.Heuristic;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Getter
 public class AlgorithmAstar extends Algorithm {
     private Heuristic heuristic;
     private Map<Integer, State> statesToVisitWithHeuristicNumber;
@@ -16,6 +18,24 @@ public class AlgorithmAstar extends Algorithm {
     }
     @Override
     public int[][] solve() {
+        this.statesToVisitWithHeuristicNumber.put(Integer.MAX_VALUE, super.getCurrentState());
+        while (this.getStatesToVisitWithHeuristicNumber().isEmpty() && !super.isSolved()) {
+            State newState ;//= findFrameWithTheLowestHeuristic();
+            this.getVisitedStates().add(newState);
+            for (int directionCharIndex = 0; directionCharIndex < super.getSearchOrder().length(); directionCharIndex++) {
+                if(newState.canMoved(super.getDirectionToMove(directionCharIndex))) {
+                    State movedState = new State(newState);
+                    movedState.move(super.getDirectionToMove(directionCharIndex));
+
+                    if (!this.getVisitedStates().contains(movedState)) {
+                        this.getStatesToVisit().add(movedState);
+                        int calculatedDistance = heuristic.calculateDistance(movedState);
+                        calculatedDistance += movedState.getDepth();//tu moze byc problem
+                        this.statesToVisitWithHeuristicNumber.put(calculatedDistance, movedState);
+                    }
+                }
+            }
+        }
         return new int[0][];
     }
 }
