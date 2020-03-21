640a7b4942cef90b7ffe1923610422b917f4356f
195460 Kowalski Krystian <195460@edu.p.lodz.pl>
2020-03-21 11:28:10 +0100

    DFS algorithm added

--- Zad1/src/main/java/pl/lodz/p/it/AlgorithmFactory.java
+++ Zad1/src/main/java/pl/lodz/p/it/AlgorithmFactory.java
@@ -2,6 +2,7 @@ package pl.lodz.p.it;
 
 import pl.lodz.p.it.algorithm.Algorithm;
 import pl.lodz.p.it.algorithm.AlgorithmBFS;
+import pl.lodz.p.it.algorithm.AlgorithmDFS;
 import pl.lodz.p.it.io.FileService;
 
 public class AlgorithmFactory {
@@ -12,7 +13,7 @@ public class AlgorithmFactory {
                 return new AlgorithmBFS(fileService.getCOLUMN_NUMBER(), fileService.getROW_NUMBER(), fileService.getPuzzle(), args[1]);
             case "dfs":
             case "DFS":
-                return null; //TODO
+                return new AlgorithmDFS(fileService.getCOLUMN_NUMBER(), fileService.getROW_NUMBER(), fileService.getPuzzle(), args[1]);
             case "astr":
             case "ASTR":
                 return null; //TODO
--- Zad1/src/main/java/pl/lodz/p/it/algorithm/AlgorithmDFS.java
+++ Zad1/src/main/java/pl/lodz/p/it/algorithm/AlgorithmDFS.java
@@ -1,40 +1,58 @@
 package pl.lodz.p.it.algorithm;
 
+import lombok.Getter;
 import pl.lodz.p.it.enums.Direction;
 
-public class AlgorithmBFS extends Algorithm {
+import java.util.Stack;
+
+
+public class AlgorithmDFS  extends Algorithm {
 
     private String searchOrder;
 
-    public AlgorithmBFS(int ROW_NUMBER, int COLUMN_NUMBER, int[][] puzzles, String searchOrder) {
+    public Stack<State> getStates() {
+        return states;
+    }
+
+    private Stack<State> states;
+
+    private final static int MAX_DEPTH = 20;
+
+    public AlgorithmDFS(int ROW_NUMBER, int COLUMN_NUMBER, int[][] puzzles, String searchOrder) {
         super(ROW_NUMBER, COLUMN_NUMBER, puzzles);
         this.searchOrder = searchOrder.toUpperCase();
+        this.states = new Stack<>();
     }
 
     @Override
     public int[][] solve() {
-        this.getStatesToVisit().add(super.getCurrentState());
-        while (!this.getStatesToVisit().isEmpty() && !this.isSolved()) {
-            State newState = this.getStatesToVisit().poll();
+        this.states.add(super.getCurrentState());
+
+        while (!this.getStates().isEmpty() && !this.isSolved()) {
+            State newState = states.pop().clone();
+
+            if(this.getVisitedStates().contains((newState))){
+                continue;
+            }
+
             this.getVisitedStates().add(newState);
+
+            if(newState.getSolutionSteps().length() >= MAX_DEPTH){
+                continue;
+            }
+
             for (int directionCharIndex = 0; directionCharIndex < searchOrder.length(); directionCharIndex++) {
                 if (newState.canMoved(this.getDirectionToMove(directionCharIndex))) {
                     State movedState = newState.clone();
                     movedState.move(this.getDirectionToMove(directionCharIndex));
                     this.setCurrentState(movedState);
+
                     if (!this.getVisitedStates().contains(movedState)) {
-                        this.getStatesToVisit().add(movedState);
-                    }
-                    if (this.isSolved()) {
-                        System.out.println(this.getCurrentState().getSolutionSteps());
-                        System.out.println(this.getCurrentState().getDepth());
-                        this.getCurrentState().printPuzzle();
-                        return super.getCurrentState().getPuzzle();
+                        this.states.add(movedState);
                     }
                 }
             }
         }
-
         System.out.println(this.getCurrentState().getSolutionSteps());
         System.out.println(this.getCurrentState().getDepth());
         this.getCurrentState().printPuzzle();
@@ -60,3 +78,4 @@ public class AlgorithmBFS extends Algorithm {
         }
     }
 }
+
