c0f0a05e8ce5c76ebaf65ee8ae54e4aaf6070e4b
Szymon Gruda <216773@edu.p.lodz.pl>
2020-03-21 15:35:59 +0100

    Add implementation of Hamming Heuristic.

--- Zad1/src/main/java/pl/lodz/p/it/algorithm/heuristic/HammingHeuristic.java
+++ Zad1/src/main/java/pl/lodz/p/it/algorithm/heuristic/HammingHeuristic.java
@@ -5,6 +5,22 @@ import pl.lodz.p.it.algorithm.State;
 public class HammingHeuristic implements Heuristic {
     @Override
     public int calculateDistance(State state) {
-        return 0;
+        int distance = 0;
+        int i = 1;
+        for (int[] tab : state.getPuzzle()) {
+            for (int value : tab) {
+                if (value == i) {
+                    distance++;
+                    if (i == 15) {
+                        int[][] puzzleTemp = state.getPuzzle();
+                        if (puzzleTemp[state.getROW_NUMBER() - 1][state.getCOLUMN_NUMBER() - 1] == 0) {
+                            distance++;
+                        }
+                    }
+                    i++;
+                }
+            }
+        }
+        return distance;
     }
 }
