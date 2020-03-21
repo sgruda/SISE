8e6fbd614f992576022b3a0fa0af347b70a8dcdd
Szymon Gruda <216773@edu.p.lodz.pl>
2020-03-21 15:03:40 +0100

    Creating interface Heuristic.

---
+++ Zad1/src/main/java/pl/lodz/p/it/algorithm/heuristic/HammingHeuristic.java
@@ -0,0 +1,10 @@
+package pl.lodz.p.it.algorithm.heuristic;
+
+import pl.lodz.p.it.algorithm.State;
+
+public class HammingHeuristic implements Heuristic {
+    @Override
+    public int calculateDistance(State state) {
+        return 0;
+    }
+}
---
+++ Zad1/src/main/java/pl/lodz/p/it/algorithm/heuristic/ManhattanHeuristic.java
@@ -0,0 +1,10 @@
+package pl.lodz.p.it.algorithm.heuristic;
+
+import pl.lodz.p.it.algorithm.State;
+
+public class ManhattanHeuristic implements Heuristic {
+    @Override
+    public int calculateDistance(State state) {
+        return 0;
+    }
+}
